832ef3c9470dd1638746c0cfd20e49a91f8a2793
Dakoda Willden <dakodaw@yahoo.com>
2021-05-21 21:06:09 -0600

    Create dotnet 5 project and start copying something over

---
+++ BudgetApiNew/Budgeting/Services/IBudgetService.cs
@@ -0,0 +1,20 @@
+﻿using BudgetApi.Models;
+using BudgetApi.Shared;
+using System;
+using System.Collections.Generic;
+
+namespace BudgetApi.Budgeting.Services
+{
+    public interface IBudgetService
+    {
+        List<BudgetTypes> GetBudgetTypes();
+        List<BudgetWithPurchaseInfo> GetBudgetLines(DateTime monthYear);
+        bool AddUpdateBudget(Budget inputBudget, int budgetId = -1);
+        bool DeleteBudgetEntry(int budgetId);
+        BudgetInfo GetExistingBudget(int budgetId);
+        double ScenarioCheck(ScenarioInput scenarioInput);
+        bool AddUpdateBudgetType(BudgetType budgetType, int budgetTypeId = -1);
+        bool DeleteBudgetTypeEntry(int budgetTypeId);
+        BudgetTypes GetBudgetType(int budgetTypeId);
+    }
+}
