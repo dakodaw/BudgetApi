28a30524b5220308ca34d7dc8ea4358cc4fc964d
Dakoda Willden <dakoda.willden@gmail.com>
2024-07-27 23:31:04 -0600

    Don't leave initial migration commented out for future

--- Budget.DB/Migrations/20240728044615_InitialMigration.cs
+++ Budget.DB/Migrations/20240728044615_InitialMigration.cs
@@ -11,166 +11,166 @@ namespace Budget.DB.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.CreateTable(
-            //    name: "BudgetType",
-            //    columns: table => new
-            //    {
-            //        Id = table.Column<int>(type: "int", nullable: false)
-            //            .Annotation("SqlServer:Identity", "1, 1"),
-            //        BudgetType = table.Column<string>(type: "nvarchar(max)", nullable: true)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_BudgetType", x => x.Id);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "BudgetType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BudgetType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BudgetType", x => x.Id);
+                });
 
-            //migrationBuilder.CreateTable(
-            //    name: "GiftCard",
-            //    columns: table => new
-            //    {
-            //        Id = table.Column<int>(type: "int", nullable: false)
-            //            .Annotation("SqlServer:Identity", "1, 1"),
-            //        Place = table.Column<string>(type: "nvarchar(max)", nullable: true),
-            //        InitialAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-            //        CardNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
-            //        AccessCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_GiftCard", x => x.Id);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "GiftCard",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Place = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    InitialAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CardNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AccessCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GiftCard", x => x.Id);
+                });
 
-            //migrationBuilder.CreateTable(
-            //    name: "IncomeSource",
-            //    columns: table => new
-            //    {
-            //        Id = table.Column<int>(type: "int", nullable: false)
-            //            .Annotation("SqlServer:Identity", "1, 1"),
-            //        SourceName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-            //        JobOf = table.Column<string>(type: "nvarchar(max)", nullable: true),
-            //        PositionName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-            //        ActiveJob = table.Column<bool>(type: "bit", nullable: false),
-            //        EstimatedIncome = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
-            //        PayFrequency = table.Column<string>(type: "nvarchar(max)", nullable: true)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_IncomeSource", x => x.Id);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "IncomeSource",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SourceName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    JobOf = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PositionName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ActiveJob = table.Column<bool>(type: "bit", nullable: false),
+                    EstimatedIncome = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    PayFrequency = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IncomeSource", x => x.Id);
+                });
 
-            //migrationBuilder.CreateTable(
-            //    name: "Purchases",
-            //    columns: table => new
-            //    {
-            //        Id = table.Column<int>(type: "int", nullable: false)
-            //            .Annotation("SqlServer:Identity", "1, 1"),
-            //        PurchaseTypeId = table.Column<int>(type: "int", nullable: false),
-            //        Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
-            //        Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-            //        PaymentType = table.Column<string>(type: "nvarchar(max)", nullable: true),
-            //        GiftCardId = table.Column<int>(type: "int", nullable: true),
-            //        Date = table.Column<DateTime>(type: "datetime2", nullable: false),
-            //        FutureReimbursement = table.Column<bool>(type: "bit", nullable: false)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_Purchases", x => x.Id);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "Purchases",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PurchaseTypeId = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PaymentType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GiftCardId = table.Column<int>(type: "int", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FutureReimbursement = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Purchases", x => x.Id);
+                });
 
-            //migrationBuilder.CreateTable(
-            //    name: "Settings",
-            //    columns: table => new
-            //    {
-            //        Id = table.Column<int>(type: "int", nullable: false)
-            //            .Annotation("SqlServer:Identity", "1, 1"),
-            //        KeyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-            //        Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_Settings", x => x.Id);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KeyName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
 
-            //migrationBuilder.CreateTable(
-            //    name: "Budget",
-            //    columns: table => new
-            //    {
-            //        Id = table.Column<int>(type: "int", nullable: false)
-            //            .Annotation("SqlServer:Identity", "1, 1"),
-            //        BudgetTypeId = table.Column<int>(type: "int", nullable: false),
-            //        Date = table.Column<DateTime>(type: "datetime2", nullable: false),
-            //        Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_Budget", x => x.Id);
-            //        table.ForeignKey(
-            //            name: "FK_Budget_BudgetType_BudgetTypeId",
-            //            column: x => x.BudgetTypeId,
-            //            principalTable: "BudgetType",
-            //            principalColumn: "Id",
-            //            onDelete: ReferentialAction.Cascade);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "Budget",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BudgetTypeId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Budget", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Budget_BudgetType_BudgetTypeId",
+                        column: x => x.BudgetTypeId,
+                        principalTable: "BudgetType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
 
-            //migrationBuilder.CreateTable(
-            //    name: "Income",
-            //    columns: table => new
-            //    {
-            //        Id = table.Column<int>(type: "int", nullable: false)
-            //            .Annotation("SqlServer:Identity", "1, 1"),
-            //        SourceId = table.Column<int>(type: "int", nullable: false),
-            //        SourceDetails = table.Column<string>(type: "nvarchar(max)", nullable: true),
-            //        Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-            //        Date = table.Column<DateTime>(type: "datetime2", nullable: false),
-            //        IsReimbursement = table.Column<bool>(type: "bit", nullable: false),
-            //        PurchaseId = table.Column<int>(type: "int", nullable: true),
-            //        IsCash = table.Column<bool>(type: "bit", nullable: false)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_Income", x => x.Id);
-            //        table.ForeignKey(
-            //            name: "FK_Income_IncomeSource_SourceId",
-            //            column: x => x.SourceId,
-            //            principalTable: "IncomeSource",
-            //            principalColumn: "Id",
-            //            onDelete: ReferentialAction.Cascade);
-            //    });
+            migrationBuilder.CreateTable(
+                name: "Income",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SourceId = table.Column<int>(type: "int", nullable: false),
+                    SourceDetails = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsReimbursement = table.Column<bool>(type: "bit", nullable: false),
+                    PurchaseId = table.Column<int>(type: "int", nullable: true),
+                    IsCash = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Income", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Income_IncomeSource_SourceId",
+                        column: x => x.SourceId,
+                        principalTable: "IncomeSource",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
 
-            //migrationBuilder.CreateIndex(
-            //    name: "IX_Budget_BudgetTypeId",
-            //    table: "Budget",
-            //    column: "BudgetTypeId");
+            migrationBuilder.CreateIndex(
+                name: "IX_Budget_BudgetTypeId",
+                table: "Budget",
+                column: "BudgetTypeId");
 
-            //migrationBuilder.CreateIndex(
-            //    name: "IX_Income_SourceId",
-            //    table: "Income",
-            //    column: "SourceId");
+            migrationBuilder.CreateIndex(
+                name: "IX_Income_SourceId",
+                table: "Income",
+                column: "SourceId");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            //migrationBuilder.DropTable(
-            //    name: "Budget");
+            migrationBuilder.DropTable(
+                name: "Budget");
 
-            //migrationBuilder.DropTable(
-            //    name: "GiftCard");
+            migrationBuilder.DropTable(
+                name: "GiftCard");
 
-            //migrationBuilder.DropTable(
-            //    name: "Income");
+            migrationBuilder.DropTable(
+                name: "Income");
 
-            //migrationBuilder.DropTable(
-            //    name: "Purchases");
+            migrationBuilder.DropTable(
+                name: "Purchases");
 
-            //migrationBuilder.DropTable(
-            //    name: "Settings");
+            migrationBuilder.DropTable(
+                name: "Settings");
 
-            //migrationBuilder.DropTable(
-            //    name: "BudgetType");
+            migrationBuilder.DropTable(
+                name: "BudgetType");
 
-            //migrationBuilder.DropTable(
-            //    name: "IncomeSource");
+            migrationBuilder.DropTable(
+                name: "IncomeSource");
         }
     }
 }
